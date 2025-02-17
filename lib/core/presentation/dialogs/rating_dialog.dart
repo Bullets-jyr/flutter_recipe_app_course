b4e0368f572232548dc4c9e8700c01b416f0fcc0
Bullets-jyr <jy.ryu.jiyoung@gmail.com>
2025-02-17 18:32:55 +0900

    feat: Menu - Rate Dialog

--- lib/core/presentation/dialogs/rating_dialog.dart
+++ lib/core/presentation/dialogs/rating_dialog.dart
@@ -40,6 +40,7 @@ class _RatingDialogState extends State<RatingDialog> {
         },
       ),
       actions: [
+        // Menu - Rate Dialog
         SmallButton(
           widget.actionName,
           color: ColorStyles.rating,
